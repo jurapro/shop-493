97eb16cc600dd214e9ac27fbd449a99f4f3878df
jurapro <jurapro@yandex.ru>
2021-03-26 16:18:53 +0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const host = 'http://dpaggsfj-m1.wsr.ru/api';
+const f = async (url, method = 'get', token = null, data = []) => {
+    const options = {
+        method: method.toUpperCase(),
+        headers: {
+            "Content-Type": 'application/json'
+        },
+    };
+
+    if (token) {
+        options.headers['Authorization'] = `Bearer ${token}`;
+    }
+
+    if (['post', 'path'].includes(method.toLowerCase())) {
+        options.body = JSON.stringify(data);
+    }
+
+    return await fetch(`${host}/${url}`, options).then(res => res.json());
+};
